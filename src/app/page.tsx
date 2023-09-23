2d61c38a8a227bbe9d9aca72015384186f2be3ac
Luis Paredes <lparedesr@fi.uba.ar>
2023-09-23 18:16:18 -0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-"use client"
+"use client";
 
 import axios from "axios";
 
-import { useEffect, useState } from "react"
-import get_JSON, { fetch_async } from "./components/fetch_api"
+import { useEffect, useState } from "react";
+import get_JSON, { fetch_async } from "./components/fetch_api";
 
 export default function Home() {
-
   let [posts, setPosts] = useState([]);
-  const url:string = 'https://jsonplaceholder.typicode.com/posts?_limit=10';
+  const url: string = "https://jsonplaceholder.typicode.com/posts?_limit=10";
 
   /*
   const fetchPost = () => {
@@ -56,13 +55,12 @@ export default function Home() {
     });
   }, []);
 */
- 
 
   // Usando Axios
 
   // Promise Sintaxis
   const client = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com/posts"
+    baseURL: "https://jsonplaceholder.typicode.com/posts",
   });
 
   /*
@@ -74,48 +72,51 @@ export default function Home() {
     });
   }, []);
   */
- 
 
-  // Metodo 
-  useEffect(()=> {
+  // await syntaxis
+  useEffect(() => {
     const fetch_fun = async () => {
-      try{
-        const res = await client.get('?_limit=10');
+      try {
+        const res = await client.get("?_limit=10");
         setPosts(res.data);
-      }catch (error){
+      } catch (error) {
         console.log(error);
       }
-    }
+    };
     fetch_fun();
-  },[]);
+  }, []);
 
   return (
     <main>
       <h1>Hola mundo</h1>
 
       <ul>
-        <li>This is page one <a href="./page_one">Page One</a> </li>
-        <li>This is page two <a href="./page_two">Page Two</a> </li>
-        <li>This is page three <a href="./page_three">Page Three</a> </li>
+        <li>
+          This is page one <a href="./page_one">Page One</a>{" "}
+        </li>
+        <li>
+          This is page two <a href="./page_two">Page Two</a>{" "}
+        </li>
+        <li>
+          This is page three <a href="./page_three">Page Three</a>{" "}
+        </li>
       </ul>
-    
-    <h1>GET request</h1>
 
-  
-    <div className="posts-container">
-      {posts.map((post) => {
-         return (
+      <h1>GET request</h1>
+
+      <div className="posts-container">
+        {posts.map((post) => {
+          return (
             <div className="post-card" key={post.id}>
-               <h2 className="post-title">{post.title}</h2>
-               <p className="post-body">{post.body}</p>
-               <div className="button">
-               <div className="delete-btn">Delete</div>
-               </div>
+              <h2 className="post-title">{post.title}</h2>
+              <p className="post-body">{post.body}</p>
+              <div className="button">
+                <div className="delete-btn">Delete</div>
+              </div>
             </div>
-         );
-      })}
-   </div>
-  
+          );
+        })}
+      </div>
     </main>
-  )
+  );
 }
